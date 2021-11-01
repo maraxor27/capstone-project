0af12cd7b71e0560903e02b35b2909a7af985da3
maraxor27 <simon.laureti@gmail.com>
2021-11-01 10:16:40 -0400

    created a usertype decorator factory in python ensure only allowed user have access to specific endpoints

--- src/login.py
+++ src/login.py
@@ -5,13 +5,21 @@ from model.users import UserSchema
 
 loginBlueprint = Blueprint("loginStuff", __name__, url_prefix="/")
 
+def userTypes_required_decorator_factory(types):
+	def decorator(func):
+		def inner(*args, **kwargs):
+			if current_user.userType in types:
+				return func(*args, **kwargs)
+			return abort(401, 'Unauthorized')
+		return inner
+	return decorator
+
 @loginBlueprint.route('/login', methods=['POST'])
 def login():
 	if current_user is not None and current_user.is_authenticated:
 		return UserSchema().dump(current_user)
 	if request is None or request.json is None:
 		return abort(400, "Empty request")
-	# print("Request: ", request.json, flush=True)
 	if request.json.get('email') is None or request.json.get('password') is None:
 		return abort(400, "Email or password is missing")
 	user = User.query.filter(User.email==request.json['email'] and \
@@ -23,6 +31,7 @@ def login():
 
 @loginBlueprint.route('/logout')
 @login_required
+@userTypes_required_decorator_factory(["USER", "OWNER", "AGENT"])
 def logout():
 	logout_user()
 	return 'ok'
